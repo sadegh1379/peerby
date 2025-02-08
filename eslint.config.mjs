bc81dbda3a2b75824f23d0413bd4ca56c4270f28
Sadegh Akbari <akbarisadegh382@gmail.com>
2025-02-08 15:34:37 +0330

    fix: eslint configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,15 +2,22 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
+const __filename = fileURLToPath(
+    import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+    ...compat.extends("next/core-web-vitals", "next/typescript"),
+    {
+        rules: {
+            "@typescript-eslint/no-empty-interface": "off",
+            "@typescript-eslint/no-empty-object-type": "off"
+        },
+    },
 ];
 
 export default eslintConfig;
