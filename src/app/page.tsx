999c441c39013434ad13d1c6e3276b09ad117edd
Sadegh Akbari <akbarisadegh382@gmail.com>
2025-02-08 17:39:58 +0330

    fix: theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,15 @@
 
 import { Button } from "@/components/ui/button";
 import { useTheme } from "next-themes";
+import { useEffect } from "react";
 
 const HomePage = () => {
   const { setTheme, theme } = useTheme();
+
+  useEffect(() => {
+    setTheme('light')
+  }, [])
+  
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <Button onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>{theme}</Button>
