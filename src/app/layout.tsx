bfe1324c59be765a71db728df4758e739648c8e1
sadegh akbari <akbarisadegh382@gmail.com>
2025-02-14 20:29:54 +0330

    fix: font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
 import SessionProvider from "@/providers/session-provider";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -35,7 +24,7 @@ export default function RootLayout({
         <link rel="icon" href="/assets/icons/icon512_rounded.png" />
         <link rel="icon" href="/assets/icons/icon512_rounded.png" sizes="512x512" />
       </head>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`antialiased`} style={{ fontFamily: "IranSans" }}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           <SessionProvider>{children}</SessionProvider>
         </ThemeProvider>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,25 +14,33 @@ const HomePage = () => {
       {/* <SplashScreen/> */}
       {/* <Button onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>{theme}</Button> */}
       {/* header */}
-      <div style={{ backgroundImage: "url(assets/pages/home/landing-bg.jpg)", backgroundPosition:"cover" }} className="w-full h-[400px] bg-cover">
+      <div className="w-full bg-green-300 bg-cover">
         <div className="max-w-6xl  mx-auto">
           {/* navbar */}
-          <div className="flex justify-between p-4 bg-green-600">
+          <div className="flex justify-between items-center p-4 bg-green-400 text-white">
             {/* avatar */}
             <div className="flex gap-2 items-center">
-              <Image className="rounded-full" width={40} height={40} src="/assets/icons/images.png" alt="" />
-              <h1 className="text-2xl font-semibold">Peerby</h1>
+              <Image className="rounded-full" width={30} height={30} src="/assets/icons/images.png" alt="" />
+              <h1 className="text-xl font-semibold">Peerby</h1>
             </div>
             {/* search box */}
             <div className="bg-white w-[400px] flex px-4 py-2 rounded-lg items-center ">
               <Search size={20} className="text-muted-foreground" />
-              <p className="text-muted-foreground ml-3">Wat wil je lenen?</p>
+              <p className="text-muted-foreground ml-3">چه چیزی میخوای?</p>
             </div>
             {/* login profile */}
-            <UserRound strokeWidth={3} size={28} />
+            <UserRound strokeWidth={3} size={20} />
+          </div>
+
+          {/* body */}
+          <div className="text-center bg-green-400 pt-14">
+            <h3 className="text-3xl font-extrabold text-white">از طریق اشتراک گذاری وصل شوید</h3>
+            <p className="mt-3 text-muted text-white">از افراد نزدیکتان چیزهای مفید قرض دهید، قرض بگیرید و اجاره کنید</p>
           </div>
         </div>
       </div>
+
+
     </div>
   );
 };
