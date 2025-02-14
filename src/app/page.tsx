79679fc7125da71b861fd5c7edb743495bbcdc62
sadegh akbari <akbarisadegh382@gmail.com>
2025-02-14 21:14:03 +0330

    fix: added landing header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,46 +1,56 @@
 "use client";
 
-import { Search, User, UserRound } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { SplashScreen } from "@/components/ui/splash-screen";
+import { Moon, Search, Sun, User, UserRound } from "lucide-react";
 import { useTheme } from "next-themes";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 const HomePage = () => {
   const { setTheme, theme } = useTheme();
-  const router = useRouter();
 
+
+  const changeThemeHandler = (theme: 'light' | 'dark') => {
+    setTheme(theme)
+  }
   return (
     <div className="font-[family-name:var(--font-geist-sans)]">
       {/* <SplashScreen/> */}
-      {/* <Button onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>{theme}</Button> */}
       {/* header */}
-      <div className="w-full bg-green-300 bg-cover">
+      <div className="w-full  bg-cover ">
         <div className="max-w-6xl  mx-auto">
           {/* navbar */}
-          <div className="flex justify-between items-center p-4 bg-green-400 text-white">
+          <div className="flex justify-between items-center p-3 rounded-sm rounded-ss-none rounded-se-none bg-green-400 text-white ">
             {/* avatar */}
             <div className="flex gap-2 items-center">
               <Image className="rounded-full" width={30} height={30} src="/assets/icons/images.png" alt="" />
-              <h1 className="text-xl font-semibold">Peerby</h1>
+              <h1 className="text-xl font-semibold hidden sm:flex">Peerby</h1>
             </div>
             {/* search box */}
-            <div className="bg-white w-[400px] flex px-4 py-2 rounded-lg items-center ">
+            <div className="bg-white w-[200px] sm:w-[400px] flex px-4 py-2 rounded-lg items-center ">
               <Search size={20} className="text-muted-foreground" />
-              <p className="text-muted-foreground ml-3">چه چیزی میخوای?</p>
+              <p className="text-muted-foreground ml-3 text-sm">چه چیزی میخوای?</p>
             </div>
             {/* login profile */}
-            <UserRound strokeWidth={3} size={20} />
+            <div className="flex items-center gap-2">
+              <UserRound strokeWidth={3} size={20} />
+              {theme === "dark" ?
+                <Sun className="cursor-pointer" onClick={() => changeThemeHandler('light')} />
+                : <Moon className="cursor-pointer" onClick={() => changeThemeHandler('dark')} />}
+
+            </div>
           </div>
 
           {/* body */}
-          <div className="text-center bg-green-400 pt-14">
-            <h3 className="text-3xl font-extrabold text-white">از طریق اشتراک گذاری وصل شوید</h3>
-            <p className="mt-3 text-muted text-white">از افراد نزدیکتان چیزهای مفید قرض دهید، قرض بگیرید و اجاره کنید</p>
+          <div className="text-center h-[70vh] p-2 sm:p-0  items-center flex flex-col justify-center gap-3">
+            <h3 className="text-3xl font-extrabold ">از طریق اشتراک گذاری وصل شوید</h3>
+            <p className="mt-3 text-lg text-muted-foreground">از افراد نزدیکتان چیزهای مفید قرض دهید، قرض بگیرید و اجاره کنید</p>
+            <Button className="bg-green-700 text-white mt-2">به محله خود بپیوندید</Button>
           </div>
+
         </div>
       </div>
-
-
     </div>
   );
 };
