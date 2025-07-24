db422e33bc1cd59b2633a470fe99ae115f214fff
sadegh akbari <akbarisadegh382@gmail.com>
2025-07-24 22:21:00 +0330

    Update dependencies in package.json and yarn.lock, and refactor LoginModal component to integrate react-hook-form with Zod validation for phone number input.

--- src/components/customize/login-modal.tsx
+++ src/components/customize/login-modal.tsx
@@ -1,20 +1,40 @@
-import type { FC } from 'react';
 import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle
-} from '../ui/dialog';
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from '@components/ui/form';
+import { Input } from '@components/ui/input';
+import { zodResolver } from '@hookform/resolvers/zod';
+import type { FC } from 'react';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
 import { Button } from '../ui/button';
+import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle } from '../ui/dialog';
 
 interface LoginModalProps {
   onClose: () => void;
   isOpen: boolean;
 }
 
+const formSchema = z.object({
+  phone: z
+    .string()
+    .min(11, { message: 'شماره تلفن صحیح نیست' })
+    .max(11, { message: 'شماره تلفن باید ۱۱ رقم باشد' })
+    .regex(/^09\d{9}$/, { message: 'شماره تلفن باید با 09 شروع شود و ۱۱ رقم باشد' })
+});
+
 const LoginModal: FC<LoginModalProps> = ({ onClose, isOpen }) => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    mode: 'onSubmit'
+  });
+
+  const onSubmit = () => {};
+
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
       <DialogContent className="sm:max-w-[425px]">
@@ -22,13 +42,29 @@ const LoginModal: FC<LoginModalProps> = ({ onClose, isOpen }) => {
           <DialogTitle>ورود</DialogTitle>
         </DialogHeader>
         <div className="text-center">
-          <h1>body</h1>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="phone"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>شماره تلفن همراه</FormLabel>
+                    <FormControl>
+                      <Input type="string" inputMode="numeric" placeholder="shadcn" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <DialogFooter>
+                <Button type="submit" className="w-full" color="">
+                  ارسال کد تایید
+                </Button>
+              </DialogFooter>
+            </form>
+          </Form>
         </div>
-        <DialogFooter>
-          <Button type="submit" className="w-full" color="">
-            Save changes
-          </Button>
-        </DialogFooter>
       </DialogContent>
     </Dialog>
   );
